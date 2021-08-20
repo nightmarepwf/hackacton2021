29a3c9a8d08be85961830358db2cbaf7d9be71fe
nightmarepwf <34105793+nightmarepwf@users.noreply.github.com>
2021-08-20 20:21:04 +0300

    youtube api

---
+++ back/Project1/Controllers/api/YoutubeController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace Project1.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YoutubeController : ControllerBase
+    {
+
+        // GET api/<DefaultController>/5
+        [HttpGet()]
+        public async Task<List<SearchResult>> Get()
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "AIzaSyCXzzGh7FprCwyzpTfWZgbUBIjTrWhhqLQ",
+                ApplicationName = this.GetType().ToString()
+            });
+
+            var searchListRequest = youtubeService.Search.List("snippet");
+            searchListRequest.Q = "Путшествие";
+            searchListRequest.MaxResults = 50;
+            searchListRequest.Type = "channel";
+            searchListRequest.RegionCode = "ru";
+            //searchListRequest.Order = SearchResource.ListRequest.OrderEnum.ViewCount;
+
+            var searchListResponse = await searchListRequest.ExecuteAsync();
+
+            List<string> videos = new List<string>();
+            List<SearchResult> channels = new List<SearchResult>();
+            List<string> playlists = new List<string>();
+
+            // Add each result to the appropriate list, and then display the lists of
+            // matching videos, channels, and playlists.
+            foreach (var searchResult in searchListResponse.Items)
+            {
+                switch (searchResult.Id.Kind)
+                {
+                    case "youtube#video":
+                        videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
+                        break;
+
+                    case "youtube#channel":
+                        channels.Add(searchResult);
+                        break;
+
+                    case "youtube#playlist":
+                        playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
+                        break;
+                }
+            }
+
+            return channels;
+        }
+    }
+       
+}
