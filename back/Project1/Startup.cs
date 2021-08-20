9b0145b2d4cdb74a024d25652717d0714562959d
nightmarepwf <34105793+nightmarepwf@users.noreply.github.com>
2021-08-20 21:21:36 +0300

    cros

--- back/Project1/Startup.cs
+++ back/Project1/Startup.cs
@@ -26,6 +26,7 @@ namespace Project1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(); // добавляем сервисы CORS
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -65,6 +66,7 @@ namespace Project1
             });
             if (env.IsDevelopment())
             {
+                app.UseCors(builder => builder.AllowAnyOrigin());
                 app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
             }
